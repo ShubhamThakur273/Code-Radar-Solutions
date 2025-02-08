bac032026720f80bd9242e2f94239f3c6ff98ed2
Shubham Thakur <shubham1534.becse24@chitkara.edu.in>
2025-02-08 13:31:53 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -9,6 +9,15 @@ int main() {
         case '+':
             printf("%d",a+b);
             break;
+        case '-':
+            printf("%d",a-b);
+            break;
+        case '*':
+            printf("%d",a*b);
+            break;
+        case '/':
+            printf("%d",a/b);
+            break;
 
     }
 }
